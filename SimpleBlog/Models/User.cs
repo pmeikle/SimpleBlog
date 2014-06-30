bd59aa99f7e79e7b6231a5965abbfb4cdda94ad0
Pierce Meikle <pierce.meikle@gmail.com>
2014-06-30 09:18:54 -0400

    Added validation logic and including bcrypt for password hashing

--- SimpleBlog/Models/User.cs
+++ SimpleBlog/Models/User.cs
@@ -5,6 +5,8 @@ namespace SimpleBlog.Models
 {
     public class User
     {
+        private const int WorkFactor = 13;
+
         public virtual int Id { get; set; }
         public virtual string Username { get; set; }
         public virtual string Email { get; set; }
@@ -12,7 +14,17 @@ namespace SimpleBlog.Models
 
         public virtual void SetPassword(string password)
         {
-            PasswordHash = "Ignoreme";
+            PasswordHash = BCrypt.Net.BCrypt.HashPassword(password, WorkFactor);
+        }
+
+        public virtual bool CheckPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, PasswordHash);
+        }
+
+        public static void FakeHash()
+        {
+            BCrypt.Net.BCrypt.HashPassword("", WorkFactor);
         }
     }
 
