fb9d312c2885219c6df66caac2ccf98a4c92a94b
Pierce Meikle <pierce.meikle@gmail.com>
2014-07-01 10:23:59 -0400

    Implemented admin posts view

---
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -0,0 +1,10 @@
+﻿using SimpleBlog.Infastructure;
+using SimpleBlog.Models;
+
+namespace SimpleBlog.Areas.Admin.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; } 
+    }
+}
