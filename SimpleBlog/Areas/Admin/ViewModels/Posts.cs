764794b38a288280c20b22d381a0d430d08df2ad
Pierce Meikle <pierce.meikle@gmail.com>
2014-07-01 11:14:18 -0400

    Can now add and edit posts

--- SimpleBlog/Areas/Admin/ViewModels/Posts.cs
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -1,4 +1,5 @@
-﻿using SimpleBlog.Infastructure;
+﻿using System.ComponentModel.DataAnnotations;
+using SimpleBlog.Infastructure;
 using SimpleBlog.Models;
 
 namespace SimpleBlog.Areas.Admin.ViewModels
@@ -6,5 +7,20 @@ namespace SimpleBlog.Areas.Admin.ViewModels
     public class PostsIndex
     {
         public PagedData<Post> Posts { get; set; } 
+    }
+
+    public class PostsForm
+    {
+        public bool IsNew { get; set; }
+        public int? PostId { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Title { get; set; }
+
+        [Required, MaxLength(128)]
+        public string Slug { get; set; }
+
+        [Required, DataType(DataType.MultilineText)]
+        public string Content { get; set; }
     }
 }
