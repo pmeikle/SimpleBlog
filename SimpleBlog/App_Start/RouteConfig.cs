dcdb37e941f3c709947378c7b39498f75abb72af
Pierce Meikle <pierce.meikle@gmail.com>
2014-07-08 11:34:10 -0400

    Added front end posts controller and views.

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 using SimpleBlog.Controllers;
 
@@ -15,6 +11,12 @@ namespace SimpleBlog
             var namespaces = new[] {typeof (PostsController).Namespace};
 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute("TagForRealThisTime", "tag/{idAndSlug}", new { controller = "Posts", action = "Tag" }, namespaces);
+            routes.MapRoute("Tag", "tag/{id}-{slug}", new {controller = "Posts", actoin = "Tag"}, namespaces);
+
+            routes.MapRoute("PostForRealThisTime", "post/{idAndSlug}", new {controller = "Posts", action = "Show"}, namespaces);
+            routes.MapRoute("Post", "post/{id}-{slug}", new{controller = "Posts", action="Show"}, namespaces);
 
             routes.MapRoute("Login", "login", new {controller = "Auth", action = "Login"}, namespaces);
             routes.MapRoute("Logout", "logout", new { controller = "Auth", action = "Logout" }, namespaces);
